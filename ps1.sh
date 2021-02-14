6fd0e26e5d8495a9ebe07c0d57b2a5516fe1328d
Joshua Matthews <jmatth93@gmail.com>
2021-02-14 00:24:12 -0600

    Create ps1.sh
    file that edits the terminal's PS1

---
+++ ps1.sh
@@ -0,0 +1,13 @@
+function working_dir() {
+    echo -e "\e[1;32m$(pwd | sed 's/\/\(.\)[a-zA-Z0-9\.\-_]*/\/\1/g')\e[m"
+}
+
+function face_display() {
+    if [[ $? == 0 ]]; then
+        echo -e "\e[0;36m(^-^)\e[m"
+    else
+        echo -e "\e[0;31m(-.-;)\e[m"
+    fi
+}
+
+export PS1="|[ \$(face_display) ]-[\e[0;34m\u\e[m\e[1;33m@\e[0;35m\h\e[m]-[\$(working_dir)]\n\\-> "
