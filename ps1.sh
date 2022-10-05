4b7c29366c74bc553c073241ad194bef8e12cc81
Joshua Matthews <jmatth93@gmail.com>
2022-10-05 17:52:35 -0500

    updated PS1

--- ps1.sh
+++ ps1.sh
@@ -1,8 +1,19 @@
-function working_dir() {
+function prompt_working_dir() {
     echo -e "\e[1;32m$(pwd | sed 's/\/\(.\)[a-zA-Z0-9\.\-_]*/\/\1/g')\e[m"
 }
 
-function face_display() {
+function prompt_machine_display_text() {
+    echo -e "\e[0;34m\u\e[m\e[1;33m@\e[0;35m\h\e[m"
+}
+
+function prompt_git_current_branch() {
+    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == "true" ]]; then
+        cur_git_branch=$(git branch --show-current)
+        echo -e "-\e[0;37;40m($cur_git_branch)\e[m"
+    fi
+}
+
+function prompt_face_display() {
     if [[ $? == 0 ]]; then
         echo -e "\e[0;36m(^-^)\e[m"
     else
@@ -10,4 +21,10 @@ function face_display() {
     fi
 }
 
-export PS1="/[ \$(face_display) ]-[\e[0;34m\u\e[m\e[1;33m@\e[0;35m\h\e[m]-[\$(working_dir)]\n\\-> "
+function prompt_ps1_text() {
+    echo -e "/[ \$(prompt_face_display) ]-[\$(prompt_machine_display_text)]-[\$(prompt_working_dir)]\$(prompt_git_current_branch)\n\\-> "
+}
+
+unset PS1
+PS1=$(prompt_ps1_text)
+
