7bb354808e0fa18e8058009265c204594048ced5
Joshua Matthews <jmatth93@gmail.com>
2024-03-31 03:05:30 -0500

    fixed clone

--- aliases.sh
+++ aliases.sh
@@ -17,7 +17,7 @@ function clean_branches() {
 
 function git_clone() {
   if [ -z $GITHUB_TOKEN ]; then
-    git clone https://github.com/$1 $HOME/git/$1
+    git clone git@github.com:$1.git $HOME/git/$1
   else
     git clone https://jmatth11:$GITHUB_TOKEN@github.com/$1 $HOME/git/$1
   fi
