8045b7ca72b10ce42c826b10aa429d00226aafba
Joshua Matthews <joshua.matthews@patracorp.com>
2025-07-22 16:56:55 -0500

    formatting

--- nvim/lua/jmatth11/plugins/dap.lua
+++ nvim/lua/jmatth11/plugins/dap.lua
@@ -23,13 +23,14 @@ return {
 
       -- JS/TS setup
       require("dap-vscode-js").setup({
-        node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-        debugger_path = "/home/jmatth11/.local/share/nvim/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-        debugger_cmd = { "extension" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+        node_path = "node",                                                                                                      -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+        debugger_path = "/home/jmatth11/.local/share/nvim/site/pack/packer/opt/vscode-js-debug",                                 -- Path to vscode-js-debug installation.
+        debugger_cmd = { "extension" },                                                                                          -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
         adapters = { 'chrome', 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'node', 'chrome' }, -- which adapters to register in nvim-dap
-        log_file_path = "(stdpath cache)/dap_vscode_js.log", -- Path for file logging
-        log_file_level = 0, -- Logging level for output to file. Set to false to disable file logging.
-        log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+        log_file_path = "(stdpath cache)/dap_vscode_js.log",                                                                     -- Path for file logging
+        log_file_level = 0,                                                                                                      -- Logging level for output to file. Set to false to disable file logging.
+        log_console_level = vim.log.levels
+        .ERROR                                                                                                                   -- Logging level for output to console. Set to false to disable console output.
       })
 
       local js_based_languages = { "typescript", "javascript", "typescriptreact" }
@@ -51,10 +52,10 @@ return {
             port = 9229,
             cwd = "${workspaceFolder}",
             runtimeArgs = {
-                '--inspect-brk',
-                './node_modules/.bin/jest',
-                '--no-coverage',
-                '--', "${file}",
+              '--inspect-brk',
+              './node_modules/.bin/jest',
+              '--no-coverage',
+              '--', "${file}",
             },
           },
           {
@@ -78,6 +79,22 @@ return {
       -- Python setup
       require("dap-python").setup("~/git/patracorp/ml-service/.venv/bin/")
 
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        ui.open({})
+      end
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+
       -- Keymaps
       vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
       vim.keymap.set('n', '<leader>B', function()
@@ -97,19 +114,6 @@ return {
       vim.keymap.set("n", "<F5>", dap.step_back)
       vim.keymap.set("n", "<F13>", dap.restart)
       vim.keymap.set('n', '<leader>ui', ui.toggle)
-
-      dap.listeners.before.attach.dapui_config = function()
-        ui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        ui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        ui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        ui.close()
-      end
     end,
   }
 }
