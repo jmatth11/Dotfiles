46663f5424ccb3603df616eac03205dcb775c85f
Joshua Matthews <jmatth93@gmail.com>
2025-07-22 22:04:03 -0500

    disable copilot at home

--- nvim/lua/jmatth11/plugins/copilot.lua
+++ nvim/lua/jmatth11/plugins/copilot.lua
@@ -4,8 +4,9 @@ return {
     dependencies = {
       "CopilotC-Nvim/CopilotChat.nvim",
       config = function()
-        require("CopilotChat").setup({})
+        -- require("CopilotChat").setup({})
       end
     },
+    enabled = not (vim.fn.isdirectory("/home/jmatth11") or vim.fn.isdirectory("/home/jmatthews"))
   }
 }
