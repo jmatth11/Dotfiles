5357030ab37488cc3f1fd57f942190f05903e9b6
Joshua Matthews <jmatth93@gmail.com>
2025-08-02 13:16:48 -0500

    fixed godot lsp attaching

--- nvim/lua/jmatth11/init.lua
+++ nvim/lua/jmatth11/init.lua
@@ -5,6 +5,16 @@ require("jmatth11.set")
 require("jmatth11.lazy")
 require("jmatth11.telescope")
 
+local project_file = vim.fn.getcwd() .. '/project.godot'
+if vim.fn.filereadable(project_file) == 1 then
+  local pipe = [[\\.\pipe\godot.pipe]]
+  local sys_info = vim.uv.os_uname()
+  if sys_info["sysname"] == "Linux" then
+    pipe = "/tmp/godot.pipe"
+  end
+  vim.fn.serverstart(pipe)
+end
+
 local autocmd = vim.api.nvim_create_autocmd
 
 -- command to delete trailing spaces
--- nvim/lua/jmatth11/plugins/lsp.lua
+++ nvim/lua/jmatth11/plugins/lsp.lua
@@ -210,9 +210,6 @@ return {
           name = "godot",
           cmd = vim.lsp.rpc.connect('127.0.0.1', 6005),
           root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
-          on_attach = function(client, bufnr)
-            vim.api.nvim_command([[echo serverstart('\\.\pipe\godot.pipe')]])
-          end
         }
 
       }
