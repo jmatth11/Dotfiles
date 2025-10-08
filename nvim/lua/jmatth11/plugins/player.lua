52f1b04a6649b51371a19b16e949873ebb22af3e
Joshua Matthews <jmatth93@gmail.com>
2025-10-08 15:47:13 -0500

    updated player plugin

--- nvim/lua/jmatth11/plugins/player.lua
+++ nvim/lua/jmatth11/plugins/player.lua
@@ -5,8 +5,9 @@ return {
   dir = "/home/jmatthews/git/jmatth11/player.nvim",
   build = "zig build",
   config = function()
-    require("player").setup({parent_dir = "/home/jmatthews/git/jmatth11/player.nvim/test"})
+    require("player").setup({parent_dir = vim.env.HOME .. "/music", recursive = true})
     nnoremap("<leader>pp", ":lua require('player').player_info()<CR>")
+    nnoremap("<leader>pf", ":lua require('player').file_select()<CR>")
   end,
   dependencies = {
     "nvim-lua/plenary.nvim", -- required for window management
