84e143a0550c040eaa14606b8820ca7f0b002153
Joshua Matthews <jmatth93@gmail.com>
2025-11-10 23:55:48 -0600

    adding nix config files

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "flake file";
+
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: 
+    let
+	system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
